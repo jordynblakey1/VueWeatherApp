9a2c178a4a8f3b3ad3ff8f0890223cb93614a78d
JordynBlakey <jordynblakey1@gmail.com>
2020-12-21 19:28:47 -0500

    All the files
    These are all the files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/weatherclip.png" width=100 height=100>
+    <Weather />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue'
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="temp">
+<p> Chicago </p>
+
+   {{weather}}
+  °F
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+    weather : "" ,
+    
+  
+    };
+  }, 
+
+  mounted() {
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=811299a5ecb45e45a50be5278b9f5ee2').
+    then(response => (this.weather = response.data.main.temp));
+    this.cityName = this.weather.main.temp; 
+   
+    //const cityWeather = JSON.parse(this.weather);
+    //this.cityName = this.weather.main.temp;
+    //console.log(cityWeather.name); 
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.temp {
+  background-color: cornsilk;
+  padding: 20px;
+  margin: 40px;
+  max-width: 100%;
+  min-width: 50%;
+  height: 90px;
+}
+</style>
